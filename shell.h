c136623affc82a41e217e82413f0d32f979dc9ac
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-05-26 06:08:14 -0700

    changes to header file

--- shell.h
+++ shell.h
@@ -13,5 +13,16 @@
 
 int exit_chckr(char *cmd, char *inp, char *arg, int status);
 char **parse(char *strng, char *inp, int cnt, char *separator, int flag);
+void c_handler(int sig);
+char *my_input(void);
+char *my_string(char *pth, char *inp, int cnt);
+char *my_check(char *inp);
+int my_strlen(char *s);
+int my_strcmp(char *k1, char *k2, int l);
+char *my_strcpy(char *dest, char *sour);
+void my_puts(char *s);
+int _putchar(char c);
+unsigned int _delimeter(char k, char *delm);
+char *my_strtok(char *s, char *delm);
 
 #endif /*SHELL_H*/
