5bbc8b6943950a79c8a6877ef6cdcecb29ef7e8b
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-05-26 09:46:50 -0700

    changes in myinput.c file

--- myinput.c
+++ myinput.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#define MAX_LENGTH 1024
 /**
  * c_handler - a function handles ctrl + c when entered by the user
  * @sig: SIGINT
@@ -10,22 +11,46 @@ void c_handler(int sig)
 	my_puts("$ ");
 }
 /**
- * my_input - A function that gets input
+ * my_input - my own getline function
  * Return: string
  */
 char *my_input(void)
 {
-	size_t n = 0;
-	ssize_t nread;
+	static char buff[MAX_LENGTH];
+	static int buff_index;
+	static int n;
 	char *line = NULL;
+	int index = 0;
 
-	nread = getline(&line, &n, stdin);
-	if (nread == -1)
+	while (1)
 	{
-		free(line);
-		return (NULL);
+		if (buff_index == n)
+		{
+			n = read(STDIN_FILENO, buff, MAX_LENGTH);
+			buff_index = 0;
+			if (n == 0)
+				break;
+		}
+		while (buff_index < n && buff[buff_index] != '\n')
+		{
+			if (buff[buff_index] == '\r')
+			{
+				buff_index++;
+				continue;
+			}
+			if (!line)
+			{
+				line = malloc(MAX_LENGTH * sizeof(char));
+			}
+			line[index++] = buff[buff_index++];
+		}
+		if (buff_index < n && buff[buff_index] == '\n')
+		{
+			line[index] = '\0';
+			buff_index++;
+			break;
+		}
 	}
-	line[nread - 1] = '\0';
 	return (line);
 }
 
